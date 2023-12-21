90f3ee40bf38f77dbcd1aad942a3303131390a79
AbdelghaniDjedidi2001 <98555048+AbdelghaniDjedidi2001@users.noreply.github.com>
2023-12-21 15:52:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ from rdf_functions.delete_all_locations import deleteAllLocations
 
 app = FastAPI()
 
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
